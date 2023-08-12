1f584bb49928c60bacc704847561fa0623fd6afc
mweiczorek <nolawnchairs@gmail.com>
2023-08-12 19:18:58 +0700

    add test case

--- test/scheduled.spec.ts
+++ test/scheduled.spec.ts
@@ -4,16 +4,22 @@ import { Async } from '../src'
 describe('Async.scheduled', () => {
 
   it('should throw an error if the date is in the past', async () => {
-    const past = new Date(Date.now() - 100)
+    const past = new Date(Date.now() - 1000)
     expect(() => Async.scheduled(past, () => { })).toThrowError()
+  })
+
+  it('should not throw an error if the date is in the future', async () => {
+    const future = new Date(Date.now() + 1000)
+    expect(() => Async.scheduled(future, () => { })).not.toThrowError()
   })
 
   it('should call the function at the specified date', async () => {
     const start = Date.now()
     const date = new Date(start + 100)
-    Async.scheduled(date, async () => {
+    Async.scheduled(date, () => {
       const end = Date.now()
       expect(end - start).toBeGreaterThanOrEqual(100)
+      expect(end - start).toBeLessThan(110)
     })
   })
 })
